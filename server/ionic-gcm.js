06ec73d8362ea2263d6a1373a560fb089c9eb4cd
praveen garg <praveengargit@gmail.com>
2016-07-19 21:18:52 +0530

    update2

--- server/ionic-gcm.js
+++ server/ionic-gcm.js
@@ -7,7 +7,7 @@ var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 var server = app.listen(3000, function(){
